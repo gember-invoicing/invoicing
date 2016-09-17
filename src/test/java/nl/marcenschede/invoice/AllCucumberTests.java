105b5a715ecb1a0481d7e0b829d7a34684d90842
Marc Enschede <marc@marcenschede.nl>
2016-09-17 20:51:38 +0200

    Setup skeleton

---
+++ src/test/java/nl/marcenschede/invoice/AllCucumberTests.java
@@ -0,0 +1,16 @@
+package nl.marcenschede.invoice;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        tags = "~@not_implemented",
+        format = {"pretty", "html:target/cucumber"},
+        glue = {"app.domain.invoice"},
+        features = "classpath:features/",
+        strict = true
+)
+public class AllCucumberTests {
+}
---
+++ src/test/java/nl/marcenschede/invoice/Glue.java
@@ -0,0 +1,4 @@
+package nl.marcenschede.invoice;
+
+public class Glue {
+}
